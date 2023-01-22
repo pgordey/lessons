4f4bb6b5f0ae5dba8079217059aa3d5018d17052
Pavel Gordeev <gordey1233@gmail.com>
2023-01-22 22:32:26 +0300

    classwork

---
+++ lesson_07/classwork_02.py
@@ -0,0 +1,27 @@
+"""
+Написать функцию, которая будет вызывать задержку выполнения программы случайным
+образом от 1-й до 5-ти секунд. Написать декоратор, который будет выводить на печать
+время выполнения этой функции (end_time - start_time).
+"""
+import random
+from datetime import datetime
+from time import sleep
+
+
+def my_decorator(func):
+    def silencer():
+        start_time = datetime.now()
+        func()
+        end_time = datetime.now()
+        print(end_time - start_time)
+    return silencer
+
+
+@my_decorator
+def my_gun():
+    delay = random.randint(1, 5)
+    sleep(delay)
+
+
+if __name__ == "__main__":
+    my_gun()
